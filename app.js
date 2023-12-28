10b5fc6459c2fdc911c37acd71dd30cefb9daff0
sutapaaditya <sutapaaditya84@gmail.com>
2023-12-28 07:51:35 +0530

    caraousel 🖼

---
+++ app.js
@@ -0,0 +1,12 @@
+let users =[
+  {
+    "pic":"./pic/bhawan.JPG",
+  },
+  ]
+function main () {
+  let idx = 0;
+  let p = document.getElementById("dp")
+  p.src = users[idx]["pic"];
+  
+}
+main();
